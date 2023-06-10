0048ce1a5c743595e6d2a47d2212cd46de6a0639
Alfe3n <115562186+Alfe3n@users.noreply.github.com>
2023-06-10 18:37:16 +0530

    test pwa 4

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,47 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
+const manifestForPlugin = {
+  registerType: "prompt",
+  includeAssets: ["vite.svg", "vite.svg", "vite,svg"],
+  manifest: {
+    name: "Weather Ups",
+    short_name: "Weathe Ups",
+    description: "An app that can show weather forecast for your city.",
+    icons: [
+      {
+        src: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
+        src: "/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+        purpose: "apple touch icon",
+      },
+      {
+        src: "/maskable_icon.png",
+        sizes: "225x225",
+        type: "image/png",
+        purpose: "any maskable",
+      },
+    ],
+    theme_color: "#171717",
+    background_color: "#e8ebf2",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "portrait",
+  },
+};
+
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+  base: "./",
+  plugins: [react(), VitePWA(manifestForPlugin)],
 });
